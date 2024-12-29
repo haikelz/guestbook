3ece355ceababd3996e8a3e9d726d36c80560853
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2024-12-29 09:46:40 +0700

    fix: constant variable typo in google secret

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -22,5 +22,5 @@ export const NEXT_PUBLIC_ADMIN_EMAIL = process.env
 export const NEXT_PUBLIC_GOOGLE_ID = process.env
   .NEXT_PUBLIC_GOOGLE_ID as string;
 export const NEXT_PUBLIC_GOOGLE_SECRET = process.env
-  .NEEXT_PUBLIC_GOOGLE_SECRET as string;
+  .NEXT_PUBLIC_GOOGLE_SECRET as string;
 export const EMAIL_NAME = process.env.EMAIL_NAME as string;
